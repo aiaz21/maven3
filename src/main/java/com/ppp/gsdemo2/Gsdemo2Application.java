4c65f014b2e89d857a8a458f392179b2eef73e1e
azai <148366585+aiaz21@users.noreply.github.com>
2024-01-17 14:29:09 +0530

    Update Gsdemo2Application.java

--- src/main/java/com/ppp/gsdemo2/Gsdemo2Application.java
+++ src/main/java/com/ppp/gsdemo2/Gsdemo2Application.java
@@ -8,6 +8,39 @@ public class Gsdemo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Gsdemo2Application.class, args);
+		checkSelectStar();
+		switchCaseRule(5);
 	}
 
+	private static void checkSelectStar() {
+		String str = "SELECT * FROM customers;";
+		System.out.println(str);
+		
+	}
+
+    public static void doSomething()
+    {
+    }
+    
+    public static int switchCaseRule(int i) {
+        switch (i)
+        {
+            case 0:
+                int j = 10;
+                int k = 20;
+
+                if (j > k*i)
+                {
+                    return k;
+                }
+
+                break;
+        }
+		return i;
+    }
+    
+    public static void foo(String bar) throws Exception
+    {
+        throw new Exception("My Message");    
+    }
 }
