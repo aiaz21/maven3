6f171e6bd0f7b55904b14c5b4aae1980370a1c93
azai <148366585+aiaz21@users.noreply.github.com>
2024-01-18 14:18:38 +0530

    Update Gsdemo2Application.java

--- src/main/java/com/ppp/gsdemo2/Gsdemo2Application.java
+++ src/main/java/com/ppp/gsdemo2/Gsdemo2Application.java
@@ -1,5 +1,6 @@
 package com.ppp.gsdemo2;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.springframework.boot.SpringApplication;
@@ -8,52 +9,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Gsdemo2Application {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Gsdemo2Application.class, args);
 		checkSelectStar();
 		switchCaseRule(5);
 		nestedIfs();
+		pickNumber();
 	}
 
 	private static void checkSelectStar() {
 		String str = "SELECT * FROM customers;";
 		System.out.println(str);
-		
+
 	}
 
-    public static void doSomething()
-    {
-    }
-    
-    public static int switchCaseRule(int i) {
-        switch (i)
-        {
-            case 0:
-                int j = 10;
-                int k = 20;
+	public static void doSomething() {
+	}
 
-                if (j > k*i)
-                {
-                    return k;
-                }
+	public static int switchCaseRule(int i) {
+		switch (i) {
+		case 0:
+			int j = 10;
+			int k = 20;
 
-                break;
-        }
+			if (j > k * i) {
+				return k;
+			}
+
+			break;
+		}
 		return i;
-    }
-    
-    public static void foo(String bar) throws Exception
-    {
-        throw new Exception("My Message");    
-    }
-    
-    public static void nestedIfs() {
-    	Random random = new Random();
-    	
-    	boolean condition1 = random.nextBoolean();
-    	boolean condition2 = random.nextBoolean();
-    	boolean condition3 = random.nextBoolean();
-    	
+	}
+
+	public static void foo(String bar) throws Exception {
+		throw new Exception("My Message");
+	}
+
+	public static void nestedIfs() {
+		 Random random = new Random();
+
+		boolean condition1 = random.nextBoolean();
+		boolean condition2 = random.nextBoolean();
+		boolean condition3 = random.nextBoolean();
+
 		if (condition1) // Compliant (depth = 1)
 		{
 			if (condition2) // Compliant (depth = 2)
@@ -67,5 +67,21 @@ public class Gsdemo2Application {
 				}
 			}
 		}
-    }
+	}
+
+	public static int pickNumber() {
+		int i = 0;
+		int j = 0;
+
+		i = i++;
+		
+		String arr = "Hello there";
+		
+		for (int p = 0; i < arr.length(); p++)
+		{
+			System.out.println("hello");
+		}
+		
+		return j++;
+	}
 }
